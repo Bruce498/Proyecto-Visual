988cdc036a31576d690898b61547a9046ad35eff
sjuri04 <86639131+sjuri04@users.noreply.github.com>
2021-11-30 23:26:16 -0300

    Add files via upload

---
+++ Eliminarmascota.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final
+{
+    class Eliminarmascota
+    {
+        public int idanimal;
+        public string nombre;
+        public string raza;
+        public string especie;
+        
+
+    }
+}
