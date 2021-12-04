63cfa53746b9b629d98122c5473eed0f5e61a75c
Bruce498 <bdvelazquez4@hotmail.com>
2021-12-03 21:57:57 -0300

    Forms PetVet

---
+++ GX_Bienvenida/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace GX_Bienvenida
+{
+    partial class GX_Bienvenida
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTNIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENID@S A PETVET";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // BTNIngresar
+            // 
+            this.BTNIngresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BTNIngresar.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.BTNIngresar.Location = new System.Drawing.Point(398, 491);
+            this.BTNIngresar.Name = "BTNIngresar";
+            this.BTNIngresar.Size = new System.Drawing.Size(162, 39);
+            this.BTNIngresar.TabIndex = 1;
+            this.BTNIngresar.Text = "INGRESAR";
+            this.BTNIngresar.UseVisualStyleBackColor = true;
+            this.BTNIngresar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GX_Bienvenida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::GX_Bienvenida.Properties.Resources.PetVetLogo1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(921, 542);
+            this.Controls.Add(this.BTNIngresar);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "GX_Bienvenida";
+            this.Text = "Veterinaria PetVet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTNIngresar;
+    }
+}
+
