4d01ee53ef2e39893013389d7dee23dada7a4bc0
sjuri04 <sjuri04@hotmail.com>
2022-01-05 16:41:42 -0300

    ...
    actualizado

---
+++ Interfaz Grafica PETVET/EliminarSocio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interfaz_Grafica_PETVET
+{
+    internal class EliminarSocio
+    {
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaz Grafica PETVET/Eliminar_Socio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Final
+{
+    public partial class Eliminar_Socio : Form
+    {
+        public Eliminar_Socio()
+        {
+            InitializeComponent();
+        }
+
+        private void Eliminar_Socio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Eliminar_Socio eliminar_Socio = new Eliminar_Socio();
+            eliminar_Socio.Show();
+            this.Dispose();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
